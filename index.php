71095701a6296356297d99640722af1d9840bc8d
Hadson Roque <hadson.roque01@git config --global user.email hadson.roque01@gmail.com>
2023-01-09 21:46:39 +0100

    first commit

---
+++ index.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+</head>
+
+<body>
+
+  <section class="paragraphLength">
+    <!-- TITOLO -->
+    <h1>
+      <?php
+      echo "Testo:";
+      ?>
+    </h1>
+
+    <!-- TESTO -->
+    <p>
+      <?php
+
+      $str = "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Doloremque necessitatibus quasi sunt aperiam ea omnis impedit maxime molestias, distinctio, harum illum, at minima voluptas iusto sed ab nostrum nemo voluptate!";
+
+      echo $str;
+      ?>
+    </p>
+
+    <!-- LUNGHEZZA TESTO -->
+    <h3>
+      <?php
+      echo "Lunghezza: " . strlen($str);
+      ?>
+    </h3>
+  </section>
+
+  <section class="censuredText">
+
+  </section>
+
+
+</body>
+
+</html>
